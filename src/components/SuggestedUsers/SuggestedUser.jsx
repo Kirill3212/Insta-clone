99ae5c67d9d1be0811bfad3a9f23b919813f9030
Kirill Terekhin <kirill.terekhin.98@bk.ru>
2023-12-11 12:06:21 +0300

    suggested users and search for user

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,36 +1,56 @@
-import { useState } from "react";
+import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
+import useFollowUser from "../../hooks/useFollowUser";
+import useAuthStore from "../../store/authStore";
+import { Link } from "react-router-dom";
 
-import { Flex, Button, Avatar, VStack, Box } from "@chakra-ui/react";
+const SuggestedUser = ({ user, setUser }) => {
+  const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(user.uid);
+  const authUser = useAuthStore((state) => state.user);
+  console.log(user);
 
-const SuggestedUser = ({ name, followers, avatar }) => {
-  const [isFollowed, setIsFollowed] = useState(false);
+  const onFollowUser = async () => {
+    await handleFollowUser();
+    setUser({
+      ...user,
+      followers: isFollowing
+        ? user.followers.filter((follower) => follower.uid !== authUser.uid)
+        : [...user.followers, authUser],
+    });
+  };
 
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar src={avatar} name={"name"} size={"md"} />
+        <Link to={`/${user.userName}`}>
+          <Avatar src={user.profilePicURL} size={"md"} />
+        </Link>
         <VStack spacing={2} alignItems={"flex-start"}>
-          <Box fontSize={12} fontWeight={"bold"}>
-            {name}
-          </Box>
+          <Link to={`/${user.userName}`}>
+            <Box fontSize={12} fontWeight={"bold"}>
+              {user.fullName}
+            </Box>
+          </Link>
           <Box fontSize={11} color={"gray.500"}>
-            {followers} followers
+            {user.followers.length} followers
           </Box>
         </VStack>
       </Flex>
-      <Button
-        fontSize={13}
-        bg={"transparent"}
-        p={0}
-        h={"max-content"}
-        fontWeight={"medium"}
-        color={"blue.400"}
-        cursor={"pointer"}
-        _hover={{ color: "white" }}
-        onClick={() => setIsFollowed(!isFollowed)}
-      >
-        {isFollowed ? "Unfollow" : "Follow"}
-      </Button>
+      {authUser.uid !== user.uid && (
+        <Button
+          fontSize={13}
+          bg={"transparent"}
+          p={0}
+          h={"max-content"}
+          fontWeight={"medium"}
+          color={"blue.400"}
+          cursor={"pointer"}
+          _hover={{ color: "white" }}
+          onClick={onFollowUser}
+          isLoading={isUpdating}
+        >
+          {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+      )}
     </Flex>
   );
 };
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,43 +1,35 @@
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
 import SuggestedHeader from "./SuggestedHeader";
 import SuggestedUser from "./SuggestedUser";
-
-import { VStack, Flex, Text, Box, Link } from "@chakra-ui/react";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 const SuggestedUsers = () => {
+  const { isLoading, suggestedUsers } = useGetSuggestedUsers();
+
+  // optional: render loading skeleton
+  if (isLoading) return null;
+
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
-
-      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-          Suggested for you
-        </Text>
-        <Text
-          fontSize={12}
-          fontWeight={"bold"}
-          color={"gray.400"}
-          cursor={"pointer"}
-        >
-          See All
-        </Text>
-      </Flex>
-
-      <SuggestedUser
-        name="Dan Abrahmov"
-        followers={1234}
-        avatar="https://bit.ly/dan-abramov"
-      />
-      <SuggestedUser
-        name="Ryan Florence"
-        followers={567}
-        avatar="https://bit.ly/ryan-florence"
-      />
-      <SuggestedUser
-        name="Christian Nwamba"
-        followers={534}
-        avatar="https://bit.ly/code-beast"
-      />
-
+      {suggestedUsers.length !== 0 && (
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+            Suggested for you
+          </Text>
+          <Text
+            fontSize={12}
+            fontWeight={"bold"}
+            _hover={{ color: "gray.400" }}
+            cursor={"pointer"}
+          >
+            See All
+          </Text>
+        </Flex>
+      )}
+      {suggestedUsers.map((user) => (
+        <SuggestedUser user={user} key={user.id} />
+      ))}
       <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
         © 2023 Build By{" "}
         <Link
@@ -49,6 +41,7 @@ const SuggestedUsers = () => {
           Kirill Terekhin
         </Link>
       </Box>
+      ;
     </VStack>
   );
 };
